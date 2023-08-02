cec98988094d5aa1690df666b4d02b9fbaa1605a
JunseoMin <minjs4562@gmail.com>
2023-08-02 14:49:08 +0900

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junseo/ros_study/sphere/src
-# Build directory: /home/junseo/ros_study/sphere/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sphere_pkg")
--- build/sphere_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junseo/ros_study/sphere/src/sphere_pkg
-# Build directory: /home/junseo/ros_study/sphere/build/sphere_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
