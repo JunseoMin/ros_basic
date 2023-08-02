e03932cdde20d1a31dd44cfb6a2ffa8ddf696d8f
JunseoMin <minjs4562@gmail.com>
2023-08-02 13:59:48 +0900

    ros noetic Topic pointcloud

---
+++ build/sphere_pkgrospy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/junseo/ros_study/sphere/src/sphere_pkgrospy
+# Build directory: /home/junseo/ros_study/sphere/build/sphere_pkgrospy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
