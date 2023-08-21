a52b4eaf14510d75ce58a3b340d1d62f33db6070
JunsoMin <minjs4562@gmail.com>
2023-08-21 17:10:21 +0900

    converting cpp file to py file (-ing) - new tesk

---
+++ ros_nav/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/junseo/ros_study/ros_nav/src
+# Build directory: /home/junseo/ros_study/ros_nav/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_nav/src/slam_gmapping/ros_gmapping/src/slam_gmapping.py
@@ -0,0 +1,153 @@
+'''
+ * slam_gmapping
+ * Copyright (c) 2008, Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* Author: Brian Gerkey */
+/* Modified by: Charles DuHadway */
+
+
+/**
+
+@mainpage slam_gmapping
+
+@htmlinclude manifest.html
+
+@b slam_gmapping is a wrapper around the GMapping SLAM library. It reads laser
+scans and odometry and computes a map. This map can be
+written to a file using e.g.
+
+  "rosrun map_server map_saver static_map:=dynamic_map"
+
+<hr>
+
+@section topic ROS topics
+
+Subscribes to (name/type):
+- @b "scan"/<a href="../../sensor_msgs/html/classstd__msgs_1_1LaserScan.html">sensor_msgs/LaserScan</a> : data from a laser range scanner 
+- @b "/tf": odometry from the robot
+
+
+Publishes to (name/type):
+- @b "/tf"/tf/tfMessage: position relative to the map
+
+
+@section services
+ - @b "~dynamic_map" : returns the map
+
+
+@section parameters ROS parameters
+
+Reads the following parameters from the parameter server
+
+Parameters used by our GMapping wrapper:
+
+- @b "~throttle_scans": @b [int] throw away every nth laser scan
+- @b "~base_frame": @b [string] the tf frame_id to use for the robot base pose
+- @b "~map_frame": @b [string] the tf frame_id where the robot pose on the map is published
+- @b "~odom_frame": @b [string] the tf frame_id from which odometry is read
+- @b "~map_update_interval": @b [double] time in seconds between two recalculations of the map
+
+
+Parameters used by GMapping itself:
+
+Laser Parameters:
+- @b "~/maxRange" @b [double] maximum range of the laser scans. Rays beyond this range get discarded completely. (default: maximum laser range minus 1 cm, as received in the the first LaserScan message)
+- @b "~/maxUrange" @b [double] maximum range of the laser scanner that is used for map building (default: same as maxRange)
+- @b "~/sigma" @b [double] standard deviation for the scan matching process (cell)
+- @b "~/kernelSize" @b [int] search window for the scan matching process
+- @b "~/lstep" @b [double] initial search step for scan matching (linear)
+- @b "~/astep" @b [double] initial search step for scan matching (angular)
+- @b "~/iterations" @b [int] number of refinement steps in the scan matching. The final "precision" for the match is lstep*2^(-iterations) or astep*2^(-iterations), respectively.
+- @b "~/lsigma" @b [double] standard deviation for the scan matching process (single laser beam)
+- @b "~/ogain" @b [double] gain for smoothing the likelihood
+- @b "~/lskip" @b [int] take only every (n+1)th laser ray for computing a match (0 = take all rays)
+- @b "~/minimumScore" @b [double] minimum score for considering the outcome of the scanmatching good. Can avoid 'jumping' pose estimates in large open spaces when using laser scanners with limited range (e.g. 5m). (0 = default. Scores go up to 600+, try 50 for example when experiencing 'jumping' estimate issues)
+
+Motion Model Parameters (all standard deviations of a gaussian noise model)
+- @b "~/srr" @b [double] linear noise component (x and y)
+- @b "~/stt" @b [double] angular noise component (theta)
+- @b "~/srt" @b [double] linear -> angular noise component
+- @b "~/str" @b [double] angular -> linear noise component
+
+Others:
+- @b "~/linearUpdate" @b [double] the robot only processes new measurements if the robot has moved at least this many meters
+- @b "~/angularUpdate" @b [double] the robot only processes new measurements if the robot has turned at least this many rads
+
+- @b "~/resampleThreshold" @b [double] threshold at which the particles get resampled. Higher means more frequent resampling.
+- @b "~/particles" @b [int] (fixed) number of particles. Each particle represents a possible trajectory that the robot has traveled
+
+Likelihood sampling (used in scan matching)
+- @b "~/llsamplerange" @b [double] linear range
+- @b "~/lasamplerange" @b [double] linear step size
+- @b "~/llsamplestep" @b [double] linear range
+- @b "~/lasamplestep" @b [double] angular step size
+
+Initial map dimensions and resolution:
+- @b "~/xmin" @b [double] minimum x position in the map [m]
+- @b "~/ymin" @b [double] minimum y position in the map [m]
+- @b "~/xmax" @b [double] maximum x position in the map [m]
+- @b "~/ymax" @b [double] maximum y position in the map [m]
+- @b "~/delta" @b [double] size of one pixel [m]
+
+
+converting cpp to python by JunseoMin
+
+'''
+#!/usr/bin/env python
+import rospy
+import time
+
+from nav_msgs.msg import MapMetaData
+from rosbag import bag
+from rosbag import view
+from boost import foreach
+
+
+
+# from gmapping import 
+
+class SlamGMapping:
+    def __init__(self) -> None:
+        
+        pass
+    
+    def updatemap(self):
+        pass
+
+    def getodompose(self):
+        pass
+    
+    def initMapper(self):
+        pass
+
+    def addscan(self):
+        pass
+
+    def compute_pose_entropy(self):
+        pass
+
